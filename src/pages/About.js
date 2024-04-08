d4b474f19017f51358ba7ed593122badb5dcd1bc
Angeloo00 <161448524+Angeloo00@users.noreply.github.com>
2024-04-08 18:51:29 +0200

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,10 @@ function About() {
             Farina&co, un ristorante fondato nel 2024, si distingue per la passione dedicata alla preparazione di ogni piatto, specialmente le pizze.
             </p>
             <p>
-            Oltre alla pizza, il menu comprende antipasti, insalate, pasta e dolci fatti in casa. Il locale è un luogo di incontro per famiglie, amici e amanti della buona cucina.
+            Oltre alla pizza, il menu comprende antipasti, insalate, pasta e dolci fatti in casa.
+            </p>
+            <p>
+            Il locale è un luogo di incontro per famiglie, amici e amanti della buona cucina.
             </p>
             <p>
             La filosofia di Farina&co è creare esperienze culinarie indimenticabili, un morso alla volta, con attenzione al dettaglio e standard di eccellenza.
